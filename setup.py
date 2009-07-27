453187185bf59bbc7413d3fc13260443015e0123
Stephan Richter <stephan.richter@gmail.com>
2009-07-27 02:17:03 +0000

    Get ready for release.

--- setup.py
+++ setup.py
@@ -1,24 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id$
+"""
 import os
 from setuptools import setup, find_packages
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(
+setup (
     name='keas.unit',
-    version = '0.1.0-dev',
-    author='Keas Inc.',
-    description="A simple wrapper around the 'units' shell command",
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    extras_require=dict(
-        test=['zope.testing'],
+    version='1.0.0',
+    author = "Stephan Richter and the Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "A simple wrapper around the 'units' shell command",
+    long_description=(
+        read('src', 'keas', 'unit', 'README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
         ),
-    install_requires=[
+    license = "ZPL 2.1",
+    keywords = "zope3 security key management infrastructure nist 800-57",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/keas.unit',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['keas'],
+    extras_require = dict(
+        test = [
+            'zope.testing',
+            ],
+        ),
+    install_requires = [
         'setuptools',
         'zope.interface',
-        'zope.schema'
+        'zope.schema',
         ],
-    include_package_data = True,
     zip_safe = False,
     )
